33d057087318b0780a7528e704fc56813a979787
Shehar-Bano <bshehar2002@gmail.com>
2024-08-08 10:23:56 +0500

    refactoring

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,38 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Medicine;
+use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ReportController extends Controller
 {
-    public function salesReport(Request $request)
-    {
-        $user = Auth::user();
-        $query = SalesOrder::query();
-
-
-        if ($request->has('orderId')) {
-            $query->where('id', 'like', '%' . $request->orderId . '%');
+    public function salesReport(){
+        $user=Auth::user();
+        return view('admin.SalesReport',compact('user'));
+    }
+    public function purchaseReport(){
+        $user=Auth::user();
+        if ($user->usertype == 'admin') {
+            $purchase = Order::get();
+        return view('admin.puchase_report',compact('user','purchase'));
         }
-
-        if ($request->has('customer')) {
-            $query->where('customer', 'like', '%' . $request->customer . '%');
+    }
+    public function stockReport(){
+        $user=Auth::user();
+        if ($user->usertype == 'admin') {
+            $medicine = Medicine::get();
+        return view('admin.stock-report',compact('user','medicine'));
         }
-
-        if ($request->has('startDate') && $request->has('endDate')) {
-            if($request->startDate==$request->endDate){
-                $query->whereDate('date','=',$request->startDate);
-            }else{
-                $query->whereBetween('date', [ $request->startDate, $request->endDate]);
-            }
-
+    }
+    public function report(){
+        $user=Auth::user();
+        if ($user->usertype == 'admin') {
+            $medicine = OrderItem::with('medicine.manufacturer')->get();
+        return view('admin.purchaseByStock',compact('user','medicine'));
         }
-
-
-
-        $orders = $query->paginate(5);
-
-        return view('admin.SalesReport', compact('user', 'orders'));
     }
 }
